dcf4de5f224ee9b64786c7b96ddd5b33259f96c0
boazpinto <pintob@gmail.com>
2019-10-16 21:18:14 +0300

    add date and time to usermessage

--- src/components/UserMessages.js
+++ src/components/UserMessages.js
@@ -5,6 +5,21 @@ import DiamondNavbar from '../components/DiamondNavbar';
 import { usersMessages } from '../Classes/Message'
 import { userDetails } from '../Classes/User'
 
+function fullMinutes(min){
+    if (min>9) return min;
+    return "0"+min;
+}
+function parseDateTime(dateTime){
+    let answer=""
+ const today=new Date();
+ if (dateTime.getFullYear()!=today.getFullYear()||dateTime.getMonth()!=today.getMonth()||dateTime.getDate()!=today.getDate()) 
+                    answer=answer+dateTime.getDate()+"/"+dateTime.getMonth()+"/"+dateTime.getFullYear()+" "
+answer=answer+dateTime.getHours()+":"+fullMinutes(dateTime.getMinutes());
+return answer;
+}
+
+
+
 export default class UserMessages extends React.Component {
     constructor(props) {
         super(props);
@@ -17,6 +32,7 @@ export default class UserMessages extends React.Component {
         }
     
     }
+
     componentDidMount(){
         this.conv.scrollTop = this.conv.scrollHeight;
         const { activeUser, allMessages,messageRead,allUsers } = this.props;
@@ -32,18 +48,21 @@ export default class UserMessages extends React.Component {
     }
     
     render() {
+        let messageDate;
         let pic;
         const { activeUser, handleLogout, allUsers, allMessages, addMessage } = this.props;
         let theSender = userDetails(this.state.id, allUsers);
         console.log(this.state.id);
         console.log("the sender");
         console.log(theSender);
+        
         if (theSender.pic) { pic = theSender.pic["_url"] } else { pic = "https://aussiegossip.com.au/wp-content/uploads/2015/11/anonymous-logo-transparent-wallpaper-4.png" }
         let showConversation = [];
         let conversation = usersMessages(activeUser, allMessages, theSender);
         conversation.messages.forEach(message => {
+            messageDate=parseDateTime(message.createdAt);
             console.log(message.from);
-            console.log(activeUser.id)
+            console.log(activeUser.id);
             if (message.from.id === activeUser.id) {
                 showConversation.push(<Row>
                     <Col xl="4" lg="4" md="4" sm="4" xs="4" ></Col>
@@ -51,6 +70,7 @@ export default class UserMessages extends React.Component {
                         <spam className="from">
                             <p>You:</p>
                             <p>{message.text}</p>
+                            <p style={{fontSize:"10px",color:"black"}}>{messageDate}</p>
                         </spam>
                     </Col>
                 </Row>)
@@ -62,6 +82,7 @@ export default class UserMessages extends React.Component {
                         <spam className="to">
                             <p>{theSender.fname}:</p>
                             <p>{message.text}</p>
+                            <p style={{fontSize:"10px",color:"black"}}>{messageDate}</p>
                         </spam>
                     </Col>
                     <Col xl="4" lg="4" md="4" sm="4" xs="4" ></Col>
@@ -72,7 +93,8 @@ export default class UserMessages extends React.Component {
             <Container id="page-container">
                 <DiamondNavbar allMessages={allMessages} activeUser={activeUser} handleLogout={handleLogout} />
 
-                <div className="userHeader"><Image style={{ width: "40px" }} src={pic}></Image>{`  ${theSender.fname} ${theSender.lname}`}</div>
+                <div className="userHeader"><Image style={{ width: "40px" }} src={pic}></Image>{`  ${theSender.fname} ${theSender.lname}`}
+                </div>
                 <Container ref={(element)=>{this.conv=element}} className="body">
                     {showConversation}
                 </Container>
