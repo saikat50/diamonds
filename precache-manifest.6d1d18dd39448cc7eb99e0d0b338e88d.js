de44fc3ea17505f538468f1ab76eb7867aa25e4b
boazpinto <pintob@gmail.com>
2019-10-27 12:24:48 +0200

    Updates

---
+++ precache-manifest.6d1d18dd39448cc7eb99e0d0b338e88d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8122135edb723238c79e83af8fa1a2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dc50b8e80f03ce05d97",
+    "url": "/static/css/2.fcd61327.chunk.css"
+  },
+  {
+    "revision": "d115912aec0241a8b75c",
+    "url": "/static/css/main.0c42a550.chunk.css"
+  },
+  {
+    "revision": "7dc50b8e80f03ce05d97",
+    "url": "/static/js/2.ddb244fd.chunk.js"
+  },
+  {
+    "revision": "d115912aec0241a8b75c",
+    "url": "/static/js/main.684e3a5a.chunk.js"
+  },
+  {
+    "revision": "2eedaeef512fcaf01d99",
+    "url": "/static/js/runtime-main.dc89dafe.js"
+  },
+  {
+    "revision": "53c9d0427ec02f50e2dd21e758a219da",
+    "url": "/static/media/1c7be669e68d05366ad16b7d2b40333b-diamond-gemstone-black-icon-by-vexels.53c9d042.png"
+  },
+  {
+    "revision": "bfb7fd2050d56d8bb4eba1b7aa3512cf",
+    "url": "/static/media/38-383271_excel-logo-png-microsoft-excel-logo-transparent-png.bfb7fd20.png"
+  },
+  {
+    "revision": "871ee0846adab9c434878ccbf2e25f58",
+    "url": "/static/media/Shapes.871ee084.png"
+  },
+  {
+    "revision": "4ca5d0189bd5825de89fe451b77a5cc3",
+    "url": "/static/media/V-512.4ca5d018.png"
+  },
+  {
+    "revision": "2c167f0bb5f0fadb1298aaf57d839227",
+    "url": "/static/media/delete-1507091-1279000.2c167f0b.png"
+  },
+  {
+    "revision": "b4bf43fd3c13b8ecad5116683f6535f0",
+    "url": "/static/media/flat_seo3-24-512.b4bf43fd.png"
+  },
+  {
+    "revision": "6b14a23ae24cb46511d2971fcd24a9ea",
+    "url": "/static/media/icons8-delete-message-26.6b14a23a.png"
+  },
+  {
+    "revision": "705a6523b46dae2bed2c24ae336d0efc",
+    "url": "/static/media/l8cb11pkn10.705a6523.jpg"
+  },
+  {
+    "revision": "7508915c0e02487798054992274c64b0",
+    "url": "/static/media/seen-300x225.7508915c.webp"
+  }
+]);
